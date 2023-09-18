b2818edda021a9374e865c716f8978d8845d967d
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-09-18 16:06:49 -0700

    Update sudo.ps1

--- sudo.ps1
+++ sudo.ps1
@@ -1,4 +1,11 @@
-Install-Module -Name NtObjectManager
+$module = Get-InstalledModule -Name 'NtObjectManager' -ErrorAction SilentlyContinue
+$username = (Get-WmiObject -Class Win32_ComputerSystem).Username | Split-Path -Leaf
+
+if (-not $module) {
+    Install-Module -Name 'NtObjectManager' -Force
+}
+
+$command = $args -join ' '
 Start-Service -Name TrustedInstaller
 $parent = Get-NtProcess -ServiceName TrustedInstaller
-$proc = New-Win32Process cmd.exe -CreationFlags NewConsole -ParentProcess $parent
+$proc = New-Win32Process "cmd.exe /c cls && cd C:\Users\$username && cmd.exe /K" -CreationFlags NewConsole -ParentProcess $parent
