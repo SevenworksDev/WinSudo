b6201d7a66d761cabc3fae4073acdb30a2caa092
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-09-19 12:44:55 -0700

    Update sudo.ps1

--- sudo.ps1
+++ sudo.ps1
@@ -5,7 +5,18 @@ if (-not $module) {
     Install-Module -Name 'NtObjectManager' -Force
 }
 
-$command = $args -join ' '
+if ($args.Count -eq 0 -or $args[0] -eq '/?') {
+    Write-Host "Usage: sudo [/s] [command]`nExample: sudo services.msc`nCommand Prompt: sudo /s`nExecutes the specified command with elevated privileges."
+    return
+}
+
+if ($args[0] -eq '/s') {
+    $command = "cmd.exe /c cls && cd C:\Users\$username && cmd.exe /K"
+}
+else {
+    $command = "cmd.exe /c $($args -join ' ') && pause"
+}
+
 Start-Service -Name TrustedInstaller
 $parent = Get-NtProcess -ServiceName TrustedInstaller
-$proc = New-Win32Process "cmd.exe /c cls && cd C:\Users\$username && cmd.exe /K" -CreationFlags NewConsole -ParentProcess $parent
+$proc = New-Win32Process $command -CreationFlags NewConsole -ParentProcess $parent
